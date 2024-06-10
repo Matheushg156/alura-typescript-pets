bae108992e992b33f34961b4e2c828b890eb83d3
matheus_gonzaga <matheushgrdc156@gmail.com>
2024-06-09 21:31:11 -0300

    inicia integração com o typeORM

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express, { Response } from "express";
 import router from "./routes";
+import "reflect-metadata";
+import { AppDataSource } from "./config/dataSource";
 
 const app = express();
 app.use(express.json());
 router(app);
+
+AppDataSource.initialize().then(() => {
+  console.log("Conectado ao banco de dados");
+}).catch((error) => {
+  console.log("Erro ao conectar ao banco de dados: ", error);
+});
 
 export default app;
